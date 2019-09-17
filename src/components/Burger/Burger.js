bc11cccca43fb447ca23c4d166939a85d12e9cef
kohichi <kohichi.horii@gmail.com>
2019-09-17 12:58:35 -0700

    outputting buger ingredient Sum dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,6 +4,12 @@ import classes from './Burger.css';
 import BurgerIngedient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+  const transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      return  [...Array(props.ingredients[igKey])].map((_, i) =>{
+        <BurgerIngedient key={igKey + i} type={igKey} />
+      });
+    });
   return(
     <div className={classes.Burger}>
       <BurgerIngedient type="bread-top" />
