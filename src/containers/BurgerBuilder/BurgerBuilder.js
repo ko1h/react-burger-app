9bd861563d9b5b102e04178ba615b5344f822a0f
kohichi <kohichi.horii@gmail.com>
2019-10-01 11:37:43 -0700

    sending  a post request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-order';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -80,8 +81,25 @@ class BurgerBuilder extends Component {
         this.setState({purschasing: false});
     }
 
-    purchaseContinueHandler = () => {
-        alert('You continue!')
+    purchaseContinueHandler = () => {        
+        // alert('You continue!')
+        const order ={
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Kohichi',
+                address: {
+                    street: 'teststreet 1',
+                    zipCode: '46521',
+                    country: 'United State'
+                },
+                email: 'test@test.com'
+            },
+            deliveryMethod: 'fastest'
+        }
+        axios.post('/orders.json', order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
     }
 
     render () {
