13a8b134afa9600e194b1893435173d71c34b738
kohichi <kohichi.horii@gmail.com>
2019-10-10 14:16:42 -0700

    navigating to contact data component

--- src/components/Order/CheckoutSummary/CheckoutSummer.js
+++ src/components/Order/CheckoutSummary/CheckoutSummer.js
@@ -7,9 +7,9 @@ import classes from './CheckoutSummary.css';
 const checkoutSummary = (props) => {
   return (
     <div className={classes.CheckoutSummary}>
-      <h1>We hope it taste well!</h1>
-      <div style={{width: '100%', margin: 'auto'}}>
-        <Burger ingredients={props.ingredients}/>
+      <h1>We hope it tastes well!</h1>
+      <div style={{ width: '100%', margin: 'auto' }}>
+        <Burger ingredients={props.ingredients} />
       </div>
       <Button
         btnType="Danger"
