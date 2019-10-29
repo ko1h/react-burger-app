eb0dd4139e29b48e8de3189239e682279051d68e
kohichi <kohichi.horii@gmail.com>
2019-10-28 17:51:46 -0700

    Add validation styling

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -66,7 +66,7 @@ class ContactData extends Component {
           placeholder: 'Your E-Mail'
         },
         value: '',
-        valibation: {
+        validation: {
           required: true
         },
         valid: false
@@ -152,6 +152,8 @@ class ContactData extends Component {
               elementType={formElement.config.elementType}
               elementConfig={formElement.config.elementConfig}
               value={formElement.config.value}
+              invalid={!formElement.config.valid}
+              shouldValidate={formElement.config.validation}
               changed={(event) =>this.inputChangeHandler(event, formElement.id)} />
         ))}
         <Button btnType="Success">ORDER</Button>
